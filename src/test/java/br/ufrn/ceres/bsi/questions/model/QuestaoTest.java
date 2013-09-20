dc9eea1e56a788d232a50e8c62f8df06acc2c954
Murilo Alfonso Peixoto <Shinigamisoul50@gmail.com>
2013-09-20 11:50:17 -0300

    Criando arquivo de teste

---
+++ src/test/java/br/ufrn/ceres/bsi/questions/model/QuestaoTest.java
@@ -0,0 +1,24 @@
+package br.ufrn.ceres.bsi.questions.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuestaoTest {
+
+	@Test
+	public void testQuestao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAlternativa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAlternativaCorreta() {
+		fail("Not yet implemented");
+	}
+
+}
